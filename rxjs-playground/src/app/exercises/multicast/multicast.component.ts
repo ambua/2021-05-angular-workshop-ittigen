92aa99fb1380f7b5a99328e081be837acde58a2e
Ambühl Adrian <adrian.ambuehl@swisscom.com>
2021-05-11 13:48:37 +0200

    rxjs: playing with multicasts

--- rxjs-playground/src/app/exercises/multicast/multicast.component.ts
+++ rxjs-playground/src/app/exercises/multicast/multicast.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Subject, BehaviorSubject, ReplaySubject, Observable } from 'rxjs';
-import { share } from 'rxjs/operators';
+import { delay, share, shareReplay, startWith } from 'rxjs/operators';
 
 import { MeasureValuesService } from './measure-values.service';
 import { ExerciseService } from '../exercise.service';
@@ -14,14 +14,21 @@ export class MulticastComponent implements OnInit {
   listeners = [];
   logStream$ = new ReplaySubject<string>();
 
-  measureValues$: Observable<number>; // später: Subject<number>;
+  measureValues$: Subject<number>; // später: Subject<number>;
 
   constructor(private mvs: MeasureValuesService, private es: ExerciseService) { }
 
   ngOnInit() {
     /******************************/
+    // this.measureValues$ = this.mvs.getValues().pipe(
+    //   // share()
+    //   shareReplay(1)
+    // );
 
-    
+    // this.measureValues$ = new Subject<number>();
+    this.measureValues$ = new ReplaySubject<number>(1);
+    this.mvs.getValues().pipe(delay(5000), startWith(999)).subscribe(this.measureValues$);
+
     /******************************/
   }
 
